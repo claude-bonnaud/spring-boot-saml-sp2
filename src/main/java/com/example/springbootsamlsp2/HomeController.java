ea0bf5b1f52b44819ccce7d6bd8e4cc65d55bceb
Claude Bonnaud <claude.bonnaud.action3d@gmail.com>
2022-10-17 13:08:55 -0400

    fix

--- src/main/java/com/example/springbootsamlsp2/HomeController.java
+++ src/main/java/com/example/springbootsamlsp2/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.springbootsamlsp2;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.saml2.provider.service.authentication.Saml2AuthenticatedPrincipal;
 import org.springframework.stereotype.Controller;
@@ -17,18 +18,21 @@ public class HomeController {
         return "home";
     }
 
+    @PreAuthorize("permitAll()")
     @GetMapping("/check")
     public String getStatusMessage(@AuthenticationPrincipal Saml2AuthenticatedPrincipal principal, Model model) {
-        return "You can display this page of spring-boot-saml-sp2";
+        return "You can display this page of spring-boot-saml-sp2 which is permitted to everybody";
     }
 
+    @PreAuthorize("hasAuthority('Everyone')")
     @GetMapping("/check2")
     public String getStatusMessage2(@AuthenticationPrincipal Saml2AuthenticatedPrincipal principal, Model model) {
-        return "You can display this page of spring-boot-saml-sp2";
+        return "You can display this page of spring-boot-saml-sp2 if you belong to the group Everyone";
     }
 
+    @PreAuthorize("hasAuthority('Foreign Exchange Transactions')") 
     @GetMapping("/check3")
     public String getStatusMessage3(@AuthenticationPrincipal Saml2AuthenticatedPrincipal principal, Model model) {
-        return "You can display this page of spring-boot-saml-sp2";
+        return "You can display this page of spring-boot-saml-sp2 if you belong to the group Foreign Exchange Transactions";
     }    
 }
